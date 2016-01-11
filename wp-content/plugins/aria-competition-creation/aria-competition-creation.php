a35f59f94e2fc68093956bc450a46f20554b0721
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-01-11 10:06:24 -0800

    Working on competition creation plugin

---
+++ wp-content/plugins/aria-competition-creation/aria-competition-creation.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name: ARIA Competion Creation
+ * Plugin URI: https://github.com/wesleykepke/ARIA
+ * Description: This plugin allows the festival chairman to create a new music competition.
+ * Version: 1.0.0 
+ * Author: KREW
+ * Author URI: https://github.com/wesleykepke/ARIA
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: aria-competition-creation
+ * Domain Path: /languages 
+ */
+
+// if the WordPress constant for the location of functions, classes, and core
+// content is not defined, then this file is not being called by WordPress
+if (!defined('WPINC')) {
+	// discontinue processing
+	die(); 
+}
+
+
+
+?>
