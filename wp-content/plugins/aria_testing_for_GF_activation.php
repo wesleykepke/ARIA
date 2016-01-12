5a2d1071b298c2e7fd6b1957b8a71015c4399507
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-01-12 13:30:55 -0800

    trying a different approach to err msgs

--- wp-content/plugins/aria_testing_for_GF_activation.php
+++ wp-content/plugins/aria_testing_for_GF_activation.php
@@ -8,6 +8,7 @@ Version: 1.2
 Author URI: http://wkepke.com
 */
 
+/*
 function aria_add_admin_notice_success() {
 	?>
 	<div class="updated notice">
@@ -30,6 +31,23 @@ function aria_add_admin_notice_error() {
 		</p>
 	</div>
 	<?php
+}
+
+function aria_activation_func() {
+	require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+	if (!is_plugin_active('gravityforms/gravityforms.php')) {  	
+		add_action('admin-notices', 'aria_add_admin_notice_error'); 
+		//do_action('admin-notices');
+		//die(); 
+	}
+}
+*/
+
+function aria_add_admin_notice_error() {
+	echo '<div class="updated>
+			<p>ARIA: Testing for Gravity Forms was not acivated; 
+			please activate the Gravity Forms plugin</p>
+			</div>';  
 }
 
 function aria_activation_func() {
