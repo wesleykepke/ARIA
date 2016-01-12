92467f7b9cb1f7a0251695d72bfcf8a51837f4aa
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-01-12 13:08:35 -0800

    now it works with two messages?

--- wp-content/plugins/aria_testing_for_GF_activation.php
+++ wp-content/plugins/aria_testing_for_GF_activation.php
@@ -4,42 +4,43 @@ Plugin Name: Aria: Testing for Gravity Forms
 Plugin URI: http://google.com
 Description: Checks to see if the Gravity Forms plugin is enabled.  
 Author: Wes
-Version: 1.1
+Version: 1.2
 Author URI: http://wkepke.com
 */
 
-function aria_add_admin_notice() {
+function aria_add_admin_notice_success() {
 	?>
-	<div class="update-nag notice">
+	<div class="updated notice">
 		<p>
 			<?php
-			_e('Gravity Forms is not activated. Please activate Gravity Forms and try again'); 
+			_e('ARIA: Testing for Gravity Forms plugin has been successfully activated. '); 
 			?>
 		</p>
 	</div>
 	<?php
 }
 
-/*
-function aria_check_GF() {
-	require_once(ABSPATH . 'wp-admin/includes/plugin.php');
-	if (is_plugin_active('gravityforms/gravityforms.php')) {
-		return true; 	
-	}
-	else {
-		return false; 
-	}
+function aria_add_admin_notice_error() {
+	?>
+	<div class="update-nag notice">
+		<p>
+			<?php
+			_e('ARIA: Testing for Gravity Forms was not acivated; please activate the Gravity Forms plugin'); 
+			?>
+		</p>
+	</div>
+	<?php
 }
-*/
 
 function aria_activation_func() {
 	require_once(ABSPATH . 'wp-admin/includes/plugin.php');
-	if (!is_plugin_active('gravityforms/gravityforms.php')) {
-		add_action('admin-notices', 'aria_add_admin_notice'); 
+	if (is_plugin_active('gravityforms/gravityforms.php')) {
+		add_action('admin-notices', 'aria_add_admin_notice_success'); 
 		do_action('admin-notices');  	
 	}
 	else {
-		echo "dmsakdnsa";
+		add_action('admin-notices', 'aria_add_admin_notice_error'); 
+		do_action('admin-notices');
 	}
 }
 
