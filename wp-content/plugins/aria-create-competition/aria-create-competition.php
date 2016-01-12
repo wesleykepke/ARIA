fd24db57a25105dfcc0634a5266e3d303261d0af
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-01-12 10:55:34 -0800

    Fixed version number

--- wp-content/plugins/aria-create-competition/aria-create-competition.php
+++ wp-content/plugins/aria-create-competition/aria-create-competition.php
@@ -9,7 +9,7 @@
  * that starts the plugin.
  *
  * @link              http://aria.cse.unr.edu
- * @since             1.0.0
+ * @since             1.0.2
  * @package           aria-create-competition
  *
  * @wordpress-plugin
