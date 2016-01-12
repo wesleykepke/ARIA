387f7998686047a47f9005998de9d49f53d2a324
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-01-12 10:27:44 -0800

    Fixed file path error

--- wp-content/plugins/aria-create-competition/includes/class-aria-create-competition.php
+++ wp-content/plugins/aria-create-competition/includes/class-aria-create-competition.php
@@ -100,24 +100,24 @@ class ARIA_Create_Competition {
 		 * The class responsible for orchestrating the actions and filters of the
 		 * core plugin.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-plugin-name-loader.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-aria-create-competition-loader.php';
 
 		/**
 		 * The class responsible for defining internationalization functionality
 		 * of the plugin.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-plugin-name-i18n.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-aria-create-competition-i18n.php';
 
 		/**
 		 * The class responsible for defining all actions that occur in the admin area.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-plugin-name-admin.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-aria-create-competition-admin.php';
 
 		/**
 		 * The class responsible for defining all actions that occur in the public-facing
 		 * side of the site.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-plugin-name-public.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-aria-create-competition-public.php';
 
 		$this->loader = new Plugin_Name_Loader();
 
