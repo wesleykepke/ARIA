80ddfbf731ae7b21c48ad1562b8c7c341411acad
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-01-12 15:56:07 -0800

    updated hook

--- wp-content/plugins/aria-create-competition/includes/class-aria-create-competition-activator.php
+++ wp-content/plugins/aria-create-competition/includes/class-aria-create-competition-activator.php
@@ -36,7 +36,7 @@ class ARIA_Create_Competition_Activator {
 		require_once(ABSPATH . 'wp-admin/includes/plugin.php');
 		if (!is_plugin_active('gravityforms/gravityforms.php')) {
 			// inform user that GravityForms must be activated
-			add_action('init', array( &$this, 'aria_admin_error_notice'));
+			add_action('admin_notices', array( &$this, 'aria_admin_error_notice_gf'));
 			die;  
 		}
 
@@ -49,12 +49,14 @@ class ARIA_Create_Competition_Activator {
 	 * @since     1.0.2
 	 * @return    void
 	 */
-	private function aria_admin_error_notice() {
+	private function aria_admin_error_notice_gf() {
 		?>
 		<div class="error notice">
 			<p>
-				<?php _e('You must have the Gravity Forms plugin enabled to create competitions.
-						Please enable GravityForms and reactivate plugin.'); ?>
+				<?php 
+					_e('You must have the Gravity Forms plugin enabled to create competitions.
+						Please enable GravityForms and reactivate plugin.'); 
+				?>
 			</p>
 		</div>
 		<?php
