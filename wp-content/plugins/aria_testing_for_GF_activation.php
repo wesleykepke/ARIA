70baf9c4ce5532af9cbde97893f82612e6f8b8af
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-01-12 12:54:56 -0800

    added register_activation_hook

--- wp-content/plugins/aria_testing_for_GF_activation.php
+++ wp-content/plugins/aria_testing_for_GF_activation.php
@@ -4,11 +4,11 @@ Plugin Name: Aria: Testing for Gravity Forms
 Plugin URI: http://google.com
 Description: Checks to see if the Gravity Forms plugin is enabled.  
 Author: Wes
-Version: 1.0
+Version: 1.1
 Author URI: http://wkepke.com
 */
 
-function aria_check_GF() {
+function aria_add_admin_notice() {
 	?>
 	<div class="update-nag notice">
 		<p>
@@ -20,11 +20,23 @@ function aria_check_GF() {
 	<?php
 }
 
-require_once(ABSPATH . 'wp-admin/includes/plugin.php');
-if (!is_plugin_active('gravityforms/gravityforms.php')) {
-	//die(); 
-	add_action('admin-notices', 'aria_check_GF'); 
-	do_action('admin-notices'); 
+function aria_check_GF() : bool {
+	require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+	if (is_plugin_active('gravityforms/gravityforms.php')) {
+		return true; 	
+	}
+	else {
+		return false; 
+	}
 }
+
+function aria_activation_func() {
+	if (!aria_check_GF()) {
+		add_action('admin-notices', 'aria_check_GF'); 
+		do_action('admin-notices'); 	
+	}
+}
+
+register_activation_hook(__FILE__, 'aria_activation_func'); 
 
 ?>
