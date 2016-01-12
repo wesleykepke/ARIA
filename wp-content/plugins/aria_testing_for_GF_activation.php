be1134b785719a1a6ddc608aef804ae76744b012
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-01-12 13:17:28 -0800

    using die() and only one function

--- wp-content/plugins/aria_testing_for_GF_activation.php
+++ wp-content/plugins/aria_testing_for_GF_activation.php
@@ -36,8 +36,8 @@ function aria_activation_func() {
 	require_once(ABSPATH . 'wp-admin/includes/plugin.php');
 	if (!is_plugin_active('gravityforms/gravityforms.php')) {  	
 		add_action('admin-notices', 'aria_add_admin_notice_error'); 
-		do_action('admin-notices');
-		die(); 
+		//do_action('admin-notices');
+		//die(); 
 	}
 }
 
