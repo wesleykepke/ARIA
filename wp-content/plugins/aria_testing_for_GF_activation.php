c9d0ae5748949c6d7f64aea0324c2b3dcb944432
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-01-12 12:42:37 -0800

    small file for testing purposes

---
+++ wp-content/plugins/aria_testing_for_GF_activation.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Aria: Testing for Gravity Forms
+Plugin URI: http://google.com
+Description: Checks to see if the Gravity Forms plugin is enabled.  
+Author: Wes
+Version: 1.0
+Author URI: http://wkepke.com
+*/
+
+function aria_check_GF() {
+	?>
+	<div class="update-nag notice">
+		<p>
+			<?php
+			_e('Gravity Forms is not activated. Please activate Gravity Forms and try again'); 
+			?>
+		</p>
+	</div>
+	<?php
+}
+
+require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+if (!is_plugin_active(gravityforms/gravityforms.php)) {
+	add_action('admin-notices', 'aria_check_GF'); 
+}
+
+?>
