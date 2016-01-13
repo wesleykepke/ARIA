f964731deacc6d7fdf744478a18d4f577b8b4a13
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-01-12 16:26:35 -0800

    add_action modified

--- wp-content/plugins/aria-create-competition/includes/class-aria-create-competition-activator.php
+++ wp-content/plugins/aria-create-competition/includes/class-aria-create-competition-activator.php
@@ -36,8 +36,8 @@ class ARIA_Create_Competition_Activator {
 		require_once(ABSPATH . 'wp-admin/includes/plugin.php');
 		if (!is_plugin_active('gravityforms/gravityforms.php')) {
 			// inform user that GravityForms must be activated
-			add_action('admin_notices', 'aria_admin_error_notice_gf');
-			do_action('admin_notices'); 
+			add_action('admin_notices', array($this, 'aria_admin_error_notice_gf'));
+			//do_action('admin_notices'); 
 			die;  
 		}
 
