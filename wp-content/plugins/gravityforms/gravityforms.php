5d9fe5ee86d5bb0cd67975a5756a52faaf364c8e
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-01-13 16:57:14 -0800

    added die

--- wp-content/plugins/gravityforms/gravityforms.php
+++ wp-content/plugins/gravityforms/gravityforms.php
@@ -332,11 +332,13 @@ class GFForms {
 			}
 		}
 
-		//if ($ARIA_plugins_are_activated) {
+		if ($ARIA_plugins_are_activated) {
 			//if (is_admin()) {
 				add_filter('gettext', array('GFForms', 'aria_inform_user_of_deactivation'), 5, 3); 
+				echo 'past the gettext filter';
+				die; 
 			//} 
-		//}
+		}
 	}
 
 	/**
