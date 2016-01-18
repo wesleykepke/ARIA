41d249dd6e33fc704c6b4fc22934454e42b9ece5
landrito <landrito@nevada.unr.edu>
2016-01-18 14:57:31 -0800

    test

--- wp-content/plugins/aria_testing_for_form.php
+++ wp-content/plugins/aria_testing_for_form.php
@@ -14,6 +14,9 @@ function aria_activation_func() {
     // Get all forms from gravity forms
     $forms = GFAPI::get_forms();
 
+
+    echo("<script>console.log('PHP: ".$forms[15]."');</script>");
+
     // Set the form index of the Competition Creation Form.
     $index = -1;
 
