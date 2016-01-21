de945bf2d83e54d1ee7e6fef62237238d0db526b
root <root@aria.cse.unr.edu>
2016-01-20 17:27:28 -0800

    can now use code to load to GF db

--- wp-content/plugins/aria_first_plugin.php
+++ wp-content/plugins/aria_first_plugin.php
@@ -11,65 +11,145 @@ Author URI: http://wkepke.com
 
 function aria_add_activation() {
 
-$new_entry = array(); 
+	$new_entry = array(); 
+	$new_entry[] = array(
+		"3" => "Level Two",
+		"4" => "Classical",
+		"1" => "Kyle's Waltz",
+		"2" => "Kyle Lee"
+	);
+	$new_entry[] = array(
+		"3" => "Level Three",
+		"4" => "Baroque",
+		"1" => "Ernest's Concerto",
+		"2" => "Ernest Lee"
+	);
+	
 
-$new_entry[] = array(
-	"Level" => "Level One",
-	"Period" => "Baroque",
-	"Song Name" => "Symphony #420",
-	"Composer" => "Bach"
-); 
+	//print_r($new_entry);
+	//die; 
 
-
-//print_r($new_entry);
-//die; 
-
-
-/*
-$success = array();
-
-$success = GFAPI::add_entries($new_entry, 17); 
-
-if (is_wp_error($success)) {
-	die($success->get_error_message()); 
-}
-
-else {
-	foreach($success as $id) {
-		echo "New entry number is: " . $id;
+	/*
+	This snippet of code will print Level, Period, Song Name, and Composer. 
+	*/
+	$form = GFFormsModel::get_form_meta(17);
+	foreach ($form['fields'] as $field) {
+		echo "Field id #$field->id has field label: $field->label </br>";
+		//echo "Field name: $field->name </br>"; prints nothing 
 	}
-	die; 
-}
-*/
+	//die("Printed all fields"); 
 
+	//  entry was added without error	
+	$success = array();
+	$success = GFAPI::add_entries($new_entry, 17); 
+	$entry_id = NULL; 
 
-// search for the newly created entry
-$search_criteria = array();
-$search_criteria['field_filters'][] = array('key' => "Level", 'value' => "Level One"); 
-$search_criteria['field_filters'][] = array('key' => "Period", 'value' => "Baroque"); 
-$search_criteria['field_filters'][] = array('key' => "Song Name", 'value' => "Symphony #420"); 
-$search_criteria['field_filters'][] = array('key' => "Composer", 'value' => "Bach"); 
+	if (is_wp_error($success)) {
+		die($success->get_error_message()); 
+	}
+	else {
+		foreach($success as $id) {
+			echo "New entry number is: " . $id;
+			$entry_id = $id; 		
+		}
+	}
 
-//$entries = GFAPI::get_entries(17, $search_criteria); 
-$entries = GFAPI::get_entries(17); 
+	// search for the newly created entry (using get_entries - plural)
+	/*
+	$search_criteria = array();
+	$search_criteria['field_filters'][] = array('key' => "Level", 'value' => "Level One"); 
+	$search_criteria['field_filters'][] = array('key' => "Period", 'value' => "Baroque"); 
+	$search_criteria['field_filters'][] = array('key' => "Song Name", 'value' => "Symphony #420"); 
+	$search_criteria['field_filters'][] = array('key' => "Composer", 'value' => "Bach"); 
 
-if (is_wp_error($entries)) {
-	die($entries->get_error_message());
-}
-else {
-	echo "There are " . count($entries) . " entries in form #17. </br>";
+	//$entries = GFAPI::get_entries(17, $search_criteria); 
+	$entries = GFAPI::get_entries(17); 
 
-	foreach($entries as $entry) {
-		//if ($entry["Song Name"] == "Symphony #420") {
+	print_r($entries[0]);
+	echo "</br>";
+	echo $entries[0][3] . "</br>" . "</br>"; 
+ 
+	//die;
+
+	if (is_wp_error($entries)) {
+		die($entries->get_error_message());
+	}
+	else {
+		echo "There are " . count($entries) . " entries in form #17. </br>";
+       if (is_wp_error($entry)) {
+                die($entry->get_error_message());
+        }
+
+$result		foreach($entries as $entry) {
+			//if ($entry["Song Name"] == "Symphony #420") {
 			echo "Matching entry has id: " . $entry["id"] . "</br>"; 
 		//}
 	}
+	
 	die("???");
+	}
+
+	// test using get_entry
+	/* this isn't working 
+	*/
+
+/*
+	$entry = GFAPI::get_entry($entry_id);
+	if (is_wp_error($entry)) {
+		die($entry->get_error_message()); 	
+	}
+	else {
+		echo "Entry id: $entry_id </br>"; 
+		echo "Should be Level: " . $entry['Level'] . "</br>"; 
+	}
+	die; 
+
+/* this works
+	$entry = GFAPI::get_entry(153);
+	if (is_wp_error($entry)) {
+		die($entry->get_error_message());
+	} 
+	else {
+		print_r($entry);
+$result		die; 
+	}
+
+*/
 }
+
+
+function aria_entry_tester() {
+	$new_entry = array(
+		"form_id" => "37",
+		"1" => "Kyle"
+	);
+
+	$success = array();
+	$success = GFAPI::add_entry($new_entry); 
+	$entry_id = NULL; 
+
+	if (is_wp_error($success)) {
+		die($success->get_error_message()); 
+	}
+	else {
+		//die("id: $success"); 
+		//foreach($success as $id) {
+		//	echo "New entry number is: " . $id;
+		//	$entry_id = $id; 		
+		//}
+		//die;
+	}
+
+	$entries = GFAPI::get_entries(37);
+	//var_dump($entries);
+	//die; 
 
 }
 
-register_activation_hook(__FILE__, "aria_add_activation"); 
+
+add_action('wp_footer', 'aria_add_activation'); 
+
+//register_activation_hook(__FILE__, "aria_entry_tester"); 
 
 
 /*
@@ -84,7 +164,8 @@ if ($success == WP_Error) {
 wp_mail works (01/19/2016)
 
 function aria_first_plugin() {
-	$random_number = mt_rand(0, 55); 
+	es);
+$random_number = mt_rand(0, 55); 
 	//echo '<h1 id="aria_first_plugin_css">This is a SUPER sweet footer that generated the random number: ' . $random_number. ' (from the range 0-55).</h1>';
 	$message = "This is a SUPER sweet footer that generated the random number: " . $random_number . " (from the range 0-55).";
 	$to = "wesleykepke@nevada.unr.edu";
@@ -96,24 +177,4 @@ function aria_first_plugin() {
 
 add_action('admin_notices', 'aria_first_plugin', 6);  
 */
-/*
-function aria_first_plugin_css() {
-	// check if language flows from right to left
-	$x = is_rtl() ? 'left' : 'right';
-	echo "
-	<style type='text/css'>
-	#aria_first_plugin_css {
-		float: $x;
-		padding-$x: 15px;
-		padding-top: 5px;
-		margin: 0;
-		font-size: 18px; 
-	}
-	</style>
-	";
-}
-
-add_action('wp_footer', 'aria_first_plugin_css'); 
-*/
-
 ?>
