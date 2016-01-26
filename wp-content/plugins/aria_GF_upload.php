adeeeeb0cd1d668e58c955fe94e3afa6d56ddbf0
root <root@aria.cse.unr.edu>
2016-01-25 18:22:30 -0800

    Upload path modification

---
+++ wp-content/plugins/aria_GF_upload.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+Plugin Name: Aria Modify GF Upload
+Plugin URI: http://google.com
+Description: Testing GF file upload. Successfully modifying file path. 
+Author: Renee
+Version: 1.1
+Author URI: http://wkepke.com
+*/
+
+
+/*
+function aria_second_plugin_css() {
+	// check if language flows from right to left
+	$x = is_rtl() ? 'left' : 'right';
+	echo "
+	<style type='text/css'>
+	#aria_second_plugin_css {
+		float: $x;
+		padding-$x: 15px;
+		padding-top: 5px;
+		margin: 0;
+		font-size: 18px; 
+	}
+	</style>
+	";
+}
+*/
+function aria_modify_upload($path_info, $form_id){
+	$path_info['path'] = '/var/www/html/wp-content/uploads/testpath/';
+	return $path_info;
+}
+
+add_filter( 'gform_upload_path', 'aria_modify_upload', 10, 2 );
+
+?>
