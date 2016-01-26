5d050463e5dfcec1f61332ce0133dffe80313df3
landrito <landrito@nevada.unr.edu>
2016-01-25 17:20:38 -0800

    test

--- wp-content/plugins/aria_testing_for_form.php
+++ wp-content/plugins/aria_testing_for_form.php
@@ -107,7 +107,7 @@ function aria_create_competition_form() {
   add_action( 'gform_after_submission_' . $result, 'aria_create_competition', 10, 2);
 
   $added_competition_creation_form = GFAPI::get_form(intval($result));
-  for ($added_competition_creation_form['confirmations'] as $key => $value) {
+  foreach ($added_competition_creation_form['confirmations'] as $key => $value) {
     $added_competition_creation_form['confirmations'][$key]['message'] 
       = "Thanks for contacting us! We will get in touch with you shortly.";
     $added_competition_creation_form['confirmations'][$key]['type'] = "message";
