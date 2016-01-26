be9bd525125044c9b674dba63625695635297656
landrito <landrito@nevada.unr.edu>
2016-01-25 17:25:08 -0800

    test

--- wp-content/plugins/aria_testing_for_form.php
+++ wp-content/plugins/aria_testing_for_form.php
@@ -106,15 +106,15 @@ function aria_create_competition_form() {
   $result = GFAPI::add_form($competition_creation_form->createFormArray());
   add_action( 'gform_after_submission_' . $result, 'aria_create_competition', 10, 2);
 
-  $added_competition_creation_form = GFAPI::get_form(intval($result));
-  // foreach ($added_competition_creation_form['confirmations'] as $key => $value) {
-  //   $added_competition_creation_form['confirmations'][$key]['message'] 
-  //     = "Thanks for contacting us! We will get in touch with you shortly.";
-  //   $added_competition_creation_form['confirmations'][$key]['type'] = "message";
-  //   break;
-  // }
+  // $added_competition_creation_form = GFAPI::get_form(intval($result));
+  // // foreach ($added_competition_creation_form['confirmations'] as $key => $value) {
+  // //   $added_competition_creation_form['confirmations'][$key]['message'] 
+  // //     = "Thanks for contacting us! We will get in touch with you shortly.";
+  // //   $added_competition_creation_form['confirmations'][$key]['type'] = "message";
+  // //   break;
+  // // }
 
-  GFAPI::update_form($added_competition_creation_form);
+  // GFAPI::update_form($added_competition_creation_form);
 
   return $result;
 }
