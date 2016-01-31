de2250cfdf5828b020fdccb49d67f8b90ed0ba4b
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-01-30 23:09:36 -0800

    should add student/teacher forms dynamically now

--- wp-content/plugins/aria_wes_create_competition.php
+++ wp-content/plugins/aria_wes_create_competition.php
@@ -4,15 +4,59 @@ Plugin Name: Aria: Create Competition (Wes)
 Plugin URI: http://google.com
 Description: This plugin will allow the festival chairman to create a competition. 
 Author: KREW (Kyle, Renee, Ernest, and Wes)
-Version: 1.0.0
+Version: 1.0.3
 Author URI: http://google.com
 */
 
 /** 
+ * This function will find the ID of the form used to create music competitions. 
+ *
+ * This function will iterate through all of the active form objects and return the 
+ * ID of the form that is used to create music competitions. If no music competition exists,
+ * the function will return -1. 
+ *
+ * @since 1.0.0
+ * @author KREW 
+ */
+function aria_get_create_competition_form_id() {
+	$create_competition_form_name = 'ARIA: Create a Competition';
+  $create_competition_form_id = NULL;
+  $all_active_forms = GFAPI::get_forms(); 
+
+  foreach ($all_active_forms as $form) {
+    if ($form['title'] === $create_competition_form_name) {
+      $create_competition_form_id = $form['id']; 
+    }
+  }
+
+  if (!isset($create_competition_form_id)) {
+    $create_competition_form_id = -1;
+  }
+
+  return $create_competition_form_id; 
+}
+
+/**
+ *
+ */
+function aria_initialize_confirmation($form_id) {
+  $added_competition_creation_form = GFAPI::get_form(intval($form_id));
+  foreach ($added_competition_creation_form['confirmations'] as $key => $value) {
+    $added_competition_creation_form['confirmations'][$key]['message'] 
+      = "Thanks for contacting us! We will get in touch with you shortly.";
+    $added_competition_creation_form['confirmations'][$key]['type'] = "message";
+    break;
+  }
+  GFAPI::update_form($added_competition_creation_form);
+}
+
+/** 
  * This function will run on the activation of this plugin. 
  *
- * This function is responsible for creating a new music competition. More specifically, this 
- * function will check to see if 
+ * This function is responsible for performing all necessary actions when activated. For
+ * example, this function will check to see if a form already exists for creating new 
+ * music competitions. If no such form exists, this function will create a new form
+ * designed specifically for creating new music competitions. 
  *
  * @since 1.0.0
  * @author KREW 
@@ -20,19 +64,10 @@ Author URI: http://google.com
 function aria_create_competition_activation() {
 	require_once(ABSPATH . 'wp-admin/includes/plugin.php');
 	if (is_plugin_active('gravityforms/gravityforms.php')) {  
-
-		/*
-		self::aria_create_teacher_form("Sample Created");
-		self::aria_create_student_form("Sample Created");
-		*/
-
-		// Get all forms from gravity forms
 		$forms = GFAPI::get_forms();
-
-		// See if the form for creating competitions exists
 		$create_competition_form_id = aria_get_create_competition_form_id();
 
-		// Form does not exist; create new form 
+		// if the form for creating music competitions does not exist, create a new form 
 		if ($create_competition_form_id === -1) {
 			aria_create_competition_form();
 		}
@@ -40,10 +75,10 @@ function aria_create_competition_activation() {
 }
 
 /** 
- * This function will run on the activation of this plugin. 
+ * This function will create a new form for creating music competitions. 
  *
- * This function is responsible for creating a new music competition. More specifically, this 
- * function will check to see if 
+ * This function is responsible for creating and adding all of the associated fields
+ * that are necessary for the festival chairman to create new music competitions. 
  *
  * @since 1.0.0
  * @author KREW 
@@ -134,10 +169,226 @@ function aria_create_competition_form() {
 }
 
 /** 
- * This function will run on the activation of this plugin. 
+ * This function will create a new form for the teachers to use to register student information. 
  *
- * This function is responsible for creating a new music competition. More specifically, this 
- * function will check to see if 
+ * This function is responsible for creating and adding all of the associated fields
+ * that are necessary for music teachers to enter data about their students that are competing. 
+ *
+ * @since 1.0.0
+ * @author KREW 
+ */
+function aria_create_teacher_form( $competition_name ) {
+  $teacher_form = new GF_Form("{$competition_name} Teacher Registration", "");
+  $field_id = 1;
+
+  $teacher_name_field = new GF_Field_Name();
+  $teacher_name_field->label = "Name";
+  $teacher_name_field->id = $field_id++;
+  $teacher_name_field->isRequired = true;
+  $teacher_form->fields[] = $teacher_name_field;
+
+  $teacher_email_field = new GF_Field_Email();
+  $teacher_email_field->label = "Email";
+  $teacher_email_field->id = $field_id++;
+  $teacher_email_field->isRequired = true;
+  $teacher_form->fields[] = $teacher_email_field;
+
+  $teacher_phone_field = new GF_Field_Phone();
+  $teacher_phone_field->label = "Phone";
+  $teacher_phone_field->id = $field_id++;
+  $teacher_phone_field->isRequired = true;
+  $teacher_form->fields[] = $teacher_phone_field;
+
+  $volunteer_preference_field = new GF_Field_Checkbox();
+  $volunteer_preference_field->label = "Volunteer Preference";
+  $volunteer_preference_field->id = $field_id++;
+  $volunteer_preference_field->isRequired = true;
+  $volunteer_preference_field->choices = array(
+    array('text' => 'Section Proctor', 'value' => 'Section Proctor', 'isSelected' => false),
+    array('text' => 'Posting Results', 'value' => 'Posting Results', 'isSelected' => false),
+    array('text' => 'Information Table', 'value' => 'Information Table', 'isSelected' => false),
+    array('text' => 'Greeting and Assisting with Locating Rooms', 'value' => 'Greeting', 'isSelected' => false),
+    array('text' => 'Hospitality (managing food in judges rooms)', 'value' => 'Hospitality', 'isSelected' => false)
+  );
+  $volunteer_preference_field->description = "Please check 1 time slot if you"
+  ." have 1-3 students competing, 2 time slots if you have 4-6 students"
+  ." competing, and 3 time slots if you have more than 6 students competing.";
+  $teacher_form->fields[] = $volunteer_preference_field;
+
+  $volunteer_time_field = new GF_Field_Checkbox();
+  $volunteer_time_field->label = "Times Available for Volunteering";
+  $volunteer_time_field->id = $field_id++;
+  $volunteer_time_field->isRequired = false;
+  $teacher_form->fields[] = $volunteer_time_field;
+
+  $student_name_field = new GF_Field_Name();
+  $student_name_field->label = "Student Name";
+  $student_name_field->id = $field_id++;
+  $student_name_field->isRequired = true;
+  $teacher_form->fields[] = $student_name_field;
+
+  $student_theory_score = new GF_Field_Number();
+  $student_theory_score->label = "Theory Score (percentage)";
+  $student_theory_score->id = $field_id++;
+  $student_theory_score->isRequired = false;
+  $student_theory_score->numberFormat = "decimal_dot";
+  $student_theory_score->rangeMin = 0;
+  $student_theory_score->rangeMax = 100;
+  $teacher_form->fields[] = $student_theory_score;
+
+  $alternate_theory_field = new GF_Field_Checkbox();
+  $alternate_theory_field->label = "Check if alternate theory exam was completed.";
+  $alternate_theory_field->id = $field_id++;
+  $alternate_theory_field->isRequired = false;
+  $alternate_theory_field->choices = array(
+    array('text' => 'Alternate theory exam completed', 'value' => 'Alternate theory exam completed', 'isSelected' => false)
+  );
+  $teacher_form->fields[] = $alternate_theory_field;
+
+  $competition_format_field = new GF_Field_Radio();
+  $competition_format_field->label = "Format of Competition";
+  $competition_format_field->id = $field_id++;
+  $competition_format_field->isRequired = false;
+  $competition_format_field->choices = $volunteer_preference_field->choices = array(
+    array('text' => 'Traditional', 'value' => 'Traditional', 'isSelected' => false),
+    array('text' => 'Competitive', 'value' => 'Competitive', 'isSelected' => false),
+    array('text' => 'Master Class (if upper level)', 'value' => 'Master Class', 'isSelected' => false)
+  );
+  $teacher_form->fields[] = $competition_format_field;
+
+  $timing_of_pieces_field = new GF_Field_Number();
+  $timing_of_pieces_field->label = "Timing of pieces (minutes)";
+  $timing_of_pieces_field->id = $field_id++;
+  $timing_of_pieces_field->isRequired = false;
+  $timing_of_pieces_field->numberFormat = "decimal_dot";
+  $teacher_form->fields[] = $timing_of_pieces_field;
+
+	// Add the new form to the festival chairman's dashboard 
+	$new_form_id = GFAPI::add_form($teacher_form->createFormArray());
+
+	// Make sure the new form was added without error
+	if (is_wp_error($new_form_id)) {
+		wp_die($new_form_id->get_error_message());
+	}
+
+  aria_initialize_confirmation($new_form_id);
+}
+
+/** 
+ * This function will create a new form for the students to use to register personal information. 
+ *
+ * This function is responsible for creating and adding all of the associated fields
+ * that are necessary for students to enter data about their upcoming music competition.  
+ *
+ * @since 1.0.0
+ * @author KREW 
+ */
+function aria_create_student_form( $competition_name ) {
+  $student_form = new GF_Form("{$competition_name} Student Registration", "");
+  $field_id = 1;
+
+  $parent_name_field = new GF_Field_Name();
+  $parent_name_field->label = "Parent Name";
+  $parent_name_field->id = $field_id++;
+  $parent_name_field->isRequired = true;
+  $parent_form->fields[] = $parent_name_field;
+
+  $parent_email_field = new GF_Field_Email();
+  $parent_email_field->label = "Parent's Email";
+  $parent_email_field->id = $field_id++;
+  $parent_email_field->isRequired = true;
+  $student_form->fields[] = $parent_email_field;
+
+  $student_name_field = new GF_Field_Name();
+  $student_name_field->label = "Student Name";
+  $student_name_field->description = "Please capitalize your child's first ".
+  "and last names and double check the spelling.  The way you type the name ".
+  "here is the way it will appear on all awards and in the Command ".
+  "Performance program.";
+  $student_name_field->id = $field_id++;
+  $student_name_field->isRequired = true;
+  $student_form->fields[] = $student_name_field;
+
+  $student_birthday_date_field = new GF_Field_Date();
+  $student_birthday_date_field->label = "Student Birthday";
+  $student_birthday_date_field->id = $field_id++;
+  $student_birthday_date_field->isRequired = true;
+  $student_birthday_date_field->calendarIconType = 'calendar';
+  $student_birthday_date_field->dateType = 'datepicker';
+  $student_form->fields[] = $student_birthday_date_field;
+
+  $piano_teachers_field = new GF_Field_Select();
+  $piano_teachers_field->label = "Piano Teacher's Name";
+  $piano_teachers_field->id = $field_id++;
+  $piano_teachers_field->isRequired = false;
+  $piano_teachers_field->description = "TBD";
+  $student_form->fields[] = $piano_teachers_field;
+
+  $teacher_missing_field = new GF_Field_Text();
+  $teacher_missing_field->label = "If your teacher's name is not listed, ".
+  "enter name below.";
+  $teacher_missing_field->id = $field_id++;
+  $teacher_missing_field->isRequired = false;
+  $student_form->fields[] = $teacher_missing_field;
+
+  $available_times = new GF_Field_Checkbox();
+  $available_times->label = "Available Festival Days (check all available times)";
+  $available_times->id = $field_id++;
+  $available_times->isRequired = true;
+  $available_times->description = "There is no guarantee that scheduling ".
+  "requests will be honored.";
+  $available_times->choices = array(
+    array('text' => 'Saturday', 'value' => 'Saturday', 'isSelected' => false),
+    array('text' => 'Sunday', 'value' => 'Sunday', 'isSelected' => false)
+  );
+  $student_form->fields[] = $available_times;
+
+  $command_times = new GF_Field_Checkbox();
+  $command_times->label = "Preferred Command Performance Time (check all available times)";
+  $command_times->id = $field_id++;
+  $command_times->isRequired = true;
+  $command_times->description = "Please check the Command Performance time ".
+  "that you prefer in the event that your child receives a superior rating.";
+  $command_times->choices = array(
+    array('text' => 'Thursday 5:30', 'value' => 'Saturday', 'isSelected' => false),
+    array('text' => 'Thursday 7:30', 'value' => 'Sunday', 'isSelected' => false)
+  );
+  $student_form->fields[] = $available_times;
+
+  $compliance_field = new GF_Field_checkbox();
+  $compliance_field->label = "Compliance Statement";
+  $compliance_field->id = $field_id++;
+  $compliance_field->isRequired = true;
+  $compliance_field->description = "As a parent, I understand and agree to ".
+  "comply with all rules, regulations, and amendments as stated in the ".
+  "Festival syllabus. I am in full compliance with the laws regarding ".
+  "photocopies and can provide verification of authentication of any legally ".
+  "printed music. I understand that adjudicator decisions are final and ".
+  "will not be contested. I know that small children may not remain in the ".
+  "room during performances of non-family members. I understand that ".
+  "requests for specific days/times will be scheduled if possible but cannot".
+  " be guaranteed.";
+  $compliance_field->choices = array(
+    array('text' => 'I have read and agree with the following statement:', 'value' => 'Agree', 'isSelected' => false),
+  );
+  $student_form->fields[] = $compliance_field;
+
+	// Add the new form to the festival chairman's dashboard 
+	$new_form_id = GFAPI::add_form($student_form->createFormArray());
+
+	// Make sure the new form was added without error
+	if (is_wp_error($new_form_id)) {
+		wp_die($new_form_id->get_error_message());
+	}
+
+  aria_initialize_confirmation($new_form_id);
+}
+
+/** 
+ * This function is responsible for adding some default address field values. 
+ *
+ * This function is used to pre-populate the address fields of a gravity form with some
+ * generic, default values. 
  *
  * @since 1.0.0
  * @author KREW 
@@ -168,54 +419,24 @@ function aria_add_default_address_inputs($field) {
 }
 
 /** 
- * This function will run on the activation of this plugin. 
+ * This function will create new registration forms for students and parents. 
  *
- * This function is responsible for creating a new music competition. More specifically, this 
- * function will check to see if 
+ * This function is responsible for creating new registration forms for both students
+ * and parents. This function will only create new registration forms for students and
+ * parents if it is used ONLY in conjunction with the form used to create new music
+ * competitions.
  *
  * @since 1.0.0
  * @author KREW 
  */
 function aria_create_competition($entry, $form ) {
 	if ($form['id'] === aria_get_create_competition_form_id()) {
-		wp_die("They match! Creating new music competition.");
-		/*
-		Aria::aria_create_student_form();
-		Aria::aria_create_teacher_form(); 
-		*/
+		aria_create_student_form();
+		aria_create_teacher_form(); 
 	}
 	else {
-		echo "form's id: " . $form['id'] . "<br>";
-		echo "global form id: " . $competition_form_id;
-		die; 
+		wp_die('No form currently exists that allows the festival chairman to create a new music competition');
 	} 
-}
-
-/** 
- * This function will find the ID of the form used to create music competitions. 
- *
- * This function will iterate through all of the active form objects and return the 
- * ID of the form that is used to create music competitions. 
- *
- * @since 1.0.0
- * @author KREW 
- */
-function aria_get_create_competition_form_id() {
-  $create_competition_form_name = 'ARIA: Create a Competition';
-  $create_competition_form_id = NULL;
-  $all_active_forms = GFAPI::get_forms(); 
-
-  foreach ($all_active_forms as $form) {
-    if ($form['title'] === $create_competition_form_name) {
-      $create_competition_form_id = $form['id']; 
-    }
-  }
-
-  if (!isset($create_competition_form_id)) {
-    $create_competition_form_id = -1;
-  }
-
-  return $create_competition_form_id; 
 }
 
 // register with the correct hooks
