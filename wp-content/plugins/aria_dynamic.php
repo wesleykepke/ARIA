ebaa5f65bebd36fe4e04bbe591a094f66a82f91d
root <root@aria.cse.unr.edu>
2016-02-02 15:33:43 -0800

    Testing plugin

---
+++ wp-content/plugins/aria_dynamic.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Aria Dynamic Population
+Plugin URI: http://google.com
+Description: Testing dynamic population with form 
+Author: Renee
+Version: 1.0
+Author URI: http://google.com
+*/
+function aria_populate_posts( $form ) {
+return $form;
+}
+
+?>
