c6d0a4b7d939e9c509cade1d357fc4c2e5d135e9
root <root@aria.cse.unr.edu>
2016-02-01 20:27:54 -0800

    Testing GET parameters

--- wp-content/plugins/aria1.js
+++ wp-content/plugins/aria1.js
@@ -1,8 +1,23 @@
 	jQuery(document).ready(function($) {
 		function getInput(){
-			$('#input_15_2').live("change", function() {
-				alert("You chose " + $('#input_2').val());
-				testPost();
+			$('#input_15_6').live("change", function() {
+	//			alert("You chose " + $('#input_15_3').val());
+				var periodStr = $('#input_15_6').val();
+				var periodInt;
+				if( periodStr == 'Baroque'){
+					periodInt = '1';
+				}
+				else if( periodStr == 'Classical'){
+					periodInt = '2';
+				}
+				else if( periodStr == 'Romantic'){
+					periodInt = '3';
+				}
+				else if( periodStr == 'Contemporary'){
+					periodInt = '4';
+				} 
+				alert("You chose " + periodStr + ": " + periodInt);
+				testGet( periodInt );
 			});
 		}
 		getInput();
@@ -19,10 +34,10 @@
                         expiration = 3600,
                         unixtime = parseInt( d.getTime() / 1000 ),
                         future_unixtime = expiration + unixtime,
-                        public_key = "0035d1a323",
-			private_key = "f2d4546aab2c06a",
-			//aria_public_key = "1ff591984b",
-                        //aria_private_key = "c4efb4676e0d6a6",
+                        //nnmta_public_key = "0035d1a323",
+			//nnmta_private_key = "f2d4546aab2c06a",
+			public_key = "1ff591984b",
+                        private_key = "c4efb4676e0d6a6",
                         route = "forms/2/entries";
 			$.post( "http://aria.cse.unr.edu/wp-content/plugins/aria_dynamic.php", {name: 'Successeded'} );
 			
@@ -38,7 +53,7 @@
 		
 
 
-		function testGet(){
+		function testGet(input){
 
 			//$.get( "http://aria.cse.unr.edu/wp-content/plugins/test.html" );
 
@@ -46,17 +61,17 @@
                         expiration = 3600,
                         unixtime = parseInt( d.getTime() / 1000 ),
                         future_unixtime = expiration + unixtime,
-                        public_key = "0035d1a323",
-			private_key = "f2d4546aab2c06a",
-			//aria_public_key = "1ff591984b",
-                        //aria_private_key = "c4efb4676e0d6a6",
+                        //nnmta_public_key = "0035d1a323",
+			//nnmta_private_key = "f2d4546aab2c06a",
+			public_key = "1ff591984b",
+                        private_key = "c4efb4676e0d6a6",
                         method = "GET",
-                        route = "forms/2/entries";
+                        route = "forms/184/entries";
 
                         stringToSign = public_key + ":" + method + ":" + route + ":" + future_unixtime;
 
                         sig = CalculateSig( stringToSign, private_key );
-                        url = "http://www.nnmta.org/gravityformsapi/" + route + "/?api_key=" + public_key;
+                        url = "http://aria.cse.unr.edu/gravityformsapi/" + route + "/?api_key=" + public_key;
 			url += "&signature=" + sig + "&expires=" + future_unixtime;
 	//		ajax_url = "/wp/gravityformsapi/" + route + "?api_key=" + public_key;
 	//		ajax_url += "&signature=" + sig + "&expires=" + future_unixtime;
@@ -72,16 +87,33 @@
 			*/
 
 			/* GET entries and alert name of students with teacher Smith, Denise */
-			
-			$.get( url, function( result ){
+		/*	var search = [ {key: 'mode', value: 'all'}, 
+					[{ key: 'key', value: '184.5'}, 
+					{key: 'operator', value: 'is'}, 
+					{key: 'value', value: input }]];
+		*/
+			var search = {
+					field_filters : [
+						{key: '5',
+						operator: 'is',
+						value: input }
+					],
+
+					mode : 'any'
+				}
+			var searchJSON = JSON.stringify( search );
+			url += '&search=' + searchJSON;// + '?paging[page_size]=200';
+			$.get( url,  function( result ){
 				var str = JSON.stringify( result );
 				var parsed = JSON.parse(str);
 				//alert (JSON.stringify(parsed['response']['entries']));	
-				parsed['response']['entries'].forEach( function(entry){
+				
+				/*parsed['response']['entries'].forEach( function(entry){
 						if( entry['15'] == "Smith, Denise" ){
 							alert( JSON.stringify(entry['1.3']) );
 						}
 					});//alert(result["response"]);
+				*/
 				});
 
 		}// end of testGet function
