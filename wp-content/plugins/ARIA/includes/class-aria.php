e3627ef0e2005e834734d7962c95e5b5f1b3fb4e
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-06 23:05:45 -0800

    trying to fix error

--- wp-content/plugins/ARIA/includes/class-aria.php
+++ wp-content/plugins/ARIA/includes/class-aria.php
@@ -124,9 +124,8 @@ class ARIA {
 		require_once("class-aria-create-competition.php");
 
 		// Register all of the hooks needed by ARIA
-
+$temp = new ARIA;
 		add_action('gform_after_submission_' . strval(aria_get_create_competition_form_id()),
-		  $temp = new ARIA;
 			array(&$temp, 'ARIA_Create_Competition::aria_create_teacher_and_student_forms'));
 
 /*
