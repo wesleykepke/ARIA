e5b2268c14bcadcdb71cad0cd4dd39ed5e3fd6d7
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-15 16:03:52 -0800

    JK aria_find_related_form_ids doesn't work

--- wp-content/plugins/ARIA/includes/class-aria-registration-handler.php
+++ wp-content/plugins/ARIA/includes/class-aria-registration-handler.php
@@ -67,9 +67,13 @@ class ARIA_Registration_Handler {
 		$teacher_form = $prepended_title . " Teacher Registration";
 		$teacher_master_form = $prepended_title . " Teacher Master";
 
+    wp_die($student_form . "..." . "Wesley 2/15/2016 Student Registration"); 
+
+    /*
     if ($student_form === "Wesley 2/15/2016 Student Registration") {
 			wp_die("Same string");
 		}
+		*/
 
 		foreach ($all_forms as $form) {
 			switch ($form["title"]) {
