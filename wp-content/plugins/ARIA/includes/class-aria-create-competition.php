12678f4c176f466b36fdd7ba5456828937f6e777
landrito <landrito@nevada.unr.edu>
2016-02-16 17:32:21 -0800

    Made names advanced `

--- wp-content/plugins/ARIA/includes/class-aria-create-competition.php
+++ wp-content/plugins/ARIA/includes/class-aria-create-competition.php
@@ -272,6 +272,7 @@ class ARIA_Create_Competition {
   }
 
   public static function aria_add_default_name_inputs($field) {
+    $field->nameFormat = 'advanced';
     $field->inputs = array(
       array(
         "id"=>"{$field->id}.2",
