e44ff5879c0f29852fddb467f76f3bf665df5e16
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-16 16:37:10 -0800

    Rewrote music csv file

--- wp-content/plugins/ARIA/includes/class-aria-music.php
+++ wp-content/plugins/ARIA/includes/class-aria-music.php
@@ -64,14 +64,11 @@ class ARIA_Music {
         // no image
         if (count($single_song_data) === $num_song_elements_no_image) {
           $all_songs[] = array (
-            '1' => $single_song_data[2],
-            '2' => $single_song_data[3],
-            '3' => $single_song_data[1],
-            '4' => $single_song_data[0],
+            '1' => $single_song_data[0],
+            '2' => $single_song_data[1],
+            '3' => $single_song_data[2],
+            '4' => $single_song_data[3],
             '5' => $single_song_data[4],
-            '6' => $single_song_data[5],
-            '7' => $single_song_data[6],
-
           );
         }
 
@@ -116,24 +113,12 @@ class ARIA_Music {
    private static function aria_create_nnmta_music_form() {
      $nnmta_music_form_name = "NNMTA Music Database";
      $nnmta_music_form = new GF_Form($nnmta_music_form_name, "");
-     //!!!$field_id_index = 1;
      $field_id_arr = self::aria_music_field_id_array();
-
-     /*
-     // NNMTA song id
-     $song_id_field = new GF_Field_Text();
-     $song_id_field->label = "NNMTA Song ID";
-     $song_id_field->id = $field_id_arr['song_id'];
-     //$field_id_index++;
-     $song_id_field->isRequired = true;
-     $nnmta_music_form->fields[] = $song_id_field;
-     */
 
      // NNMTA song name
      $song_name_field = new GF_Field_Text();
      $song_name_field->label = "Song Name";
      $song_name_field->id = $field_id_arr['song_name'];
-     //$field_id_index++;
      $song_name_field->isRequired = true;
      $nnmta_music_form->fields[] = $song_name_field;
 
@@ -141,7 +126,6 @@ class ARIA_Music {
      $song_composer_field = new GF_Field_Text();
      $song_composer_field->label = "Composer Name";
      $song_composer_field->id = $field_id_arr['song_composer'];
-     //$field_id_index++;
      $song_composer_field->isRequired = true;
      $nnmta_music_form->fields[] = $song_composer_field;
 
@@ -149,7 +133,6 @@ class ARIA_Music {
      $song_level_field = new GF_Field_Text();
      $song_level_field->label = "Song Level";
      $song_level_field->id = $field_id_arr['song_level'];
-     //$field_id_index++;
      $song_level_field->isRequired = true;
      $nnmta_music_form->fields[] = $song_level_field;
 
@@ -157,20 +140,15 @@ class ARIA_Music {
      $song_period_field = new GF_Field_Text();
      $song_period_field->label = "Song Period";
      $song_period_field->id = $field_id_arr['song_period'];
-     //$field_id_index++;
      $song_period_field->isRequired = true;
      $nnmta_music_form->fields[] = $song_period_field;
 
-     // NNMTA song image
-     /*
-     Currently GF_Field_Text but might need to change
-     */
-     $song_song_img_field = new GF_Field_Text();
-     $song_song_img_field->label = "Song Image";
-     $song_song_img_field->id = $field_id_arr['song_image'];
-     //$field_id_index++;
-     $song_song_img_field->isRequired = true;
-     $nnmta_music_form->fields[] = $song_song_img_field;
+     // NNMTA song catalog
+		 $song_catalog_field = new GF_Field_Text();
+     $song_catalog_field->label = "Song Catalog";
+     $song_catalog_field->id = $field_id_arr['song_catalog'];
+     $song_catalog_field->isRequired = false;
+     $nnmta_music_form->fields[] = $song_catalog_field;
 
      // add the new form to the festival chairman's dashboard
      $new_form_id = GFAPI::add_form($nnmta_music_form->createFormArray());
@@ -208,21 +186,28 @@ class ARIA_Music {
    * @since 1.0.0
    * @author KREW
    */
-  public static function aria_modify_upload($path_info, $form_id){
+  public static function aria_modify_upload_path($path_info, $form_id){
   	$path_info['path'] = '/var/www/html/wp-content/uploads/testpath/';
   	return $path_info;
   }
 
-	// !!! New function
+	/**
+   * This function will change the default file path for uploaded files.
+   *
+   * In order to upload music from a file, we need to know where the music
+	 * file resides. This function will set a pre-determined file path so
+	 * the music data can be read from.
+   *
+   * @since 1.0.0
+   * @author KREW
+   */
 	public static function aria_music_field_id_array() {
 		return array(
-			'song_id' => 1,
-			'song_name' => 2,
-			'song_composer' => 3,
-			'song_level' => 4,
-			'song_period' => 5,
-			'song_image' => 6
+			'song_name' => 1,
+			'song_composer' => 2,
+			'song_level' => 3,
+			'song_period' => 4,
+			'song_catalog' => 5
 		);
 	}
-
 }
