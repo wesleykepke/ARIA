7df77b85d4ef47dadbca80bdb21e981221074983
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-18 10:00:16 -0800

    Added a file to store all constants

---
+++ wp-content/plugins/ARIA/includes/aria-constants.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * The file holds all of the constants that are used throughout ARIA.
+ *
+ * This file lists various constants and what they represent. The primary
+ * purpose of this file is to be a single source of truth for all constants
+ * used throughout the program.
+ *
+ * @link       http://wesleykepke.github.io/ARIA/
+ * @since      1.0.0
+ *
+ * @package    ARIA
+ * @subpackage ARIA/includes
+ */
+
+// competitions
+define("CREATE_COMPETITION_FORM_NAME", "ARIA: Create a Competition");
+ 
