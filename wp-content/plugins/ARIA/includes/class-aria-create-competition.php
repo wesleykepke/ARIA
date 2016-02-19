78ba1bdd2f116d707a280681d491aea41d159061
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-18 17:18:58 -0800

    Trying dynamic add_action functionality

--- wp-content/plugins/ARIA/includes/class-aria-create-competition.php
+++ wp-content/plugins/ARIA/includes/class-aria-create-competition.php
@@ -53,7 +53,7 @@ class ARIA_Create_Competition {
       add_action('gform_confirmation_' . strval($form_id),
 	      array('ARIA_Create_Competition', 'aria_create_teacher_and_student_forms'),
 	      10, 1);
-      wp_die('hook added to: ' . $hook);
+      //wp_die('hook added to: ' . $hook);
     }
     else {
       wp_die('hook has already been added');
