18d753915677541773ee85291a1015f3cdf9d32c
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-18 16:29:46 -0800

    Added some constants

--- wp-content/plugins/ARIA/includes/aria-constants.php
+++ wp-content/plugins/ARIA/includes/aria-constants.php
@@ -16,3 +16,10 @@
 
 // competitions
 define("CREATE_COMPETITION_FORM_NAME", "ARIA: Create a Competition");
+define("MUSIC_UPLOAD_FORM_NAME", "ARIA: Upload Music");
+define("NNMTA_MUSIC_DATABASE_NAME", "NNMTA: Music Database");
+define("CSV_UPLOAD_FIELD_NAME", "CSV Music File");
+define("STUDENT_REG", "Student Registration");
+define("STUDENT_MASTER", "Student Master");
+define("TEACHER_REG", "Teacher Registration");
+define("TEACHER_MASTER", "Teacher Master");
